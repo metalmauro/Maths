b6723889c4842787a34647b794faacf032d980df
Matt Mauro <matt.mauro@gmail.com>
2016-11-01 12:41:52 -0400

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Matthew Mauro on 2016-11-01.
+//  Copyright © 2016 Matthew Mauro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
